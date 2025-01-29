e8df3c0a2dee4d6c7ec402c5249b7e1e145f4e68
Diogo Correia <me@diogotc.com>
2025-01-29 12:10:24 +0100

    feat(nix): add meta section to package

--- nix/package.nix
+++ nix/package.nix
@@ -1,9 +1,20 @@
-{ rustPlatform }:
-rustPlatform.buildRustPackage {
+{
+  lib,
+  rustPlatform,
+}:
+rustPlatform.buildRustPackage rec {
   pname = "infra-keyval";
   version = "0.1.0";
 
   src = ../.;
   cargoLock.lockFile = ../Cargo.lock;
-}
 
+  meta = {
+    changelog = "https://github.com/diogotcorreia/infra-keyval/releases/tag/${version}";
+    description = "Key-value store for infra-related config";
+    homepage = "https://github.com/diogotcorreia/infra-keyval";
+    license = lib.licenses.gpl3;
+    mainProgram = "infra-keyval";
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+  };
+}
